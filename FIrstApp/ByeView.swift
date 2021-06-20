b6eba62dacc9f351a6c1afba5c95e77ae855ee51
Charles Haines <crono102@gmail.com>
2021-06-20 18:30:28 -0400

    first commit initial commit

---
+++ FIrstApp/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  FIrstApp
+//
+//  Created by Charles Haines on 6/20/21.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye bye, World!");
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
